616812992deee19b6250270e161cec8780be0b3e
MOHIT DAVAR <mohitdavar2004@gmail.com>
2024-08-14 23:50:13 +0530

    Navbar

--- Project/src/components/Includes/Navbar.tsx
+++ Project/src/components/Includes/Navbar.tsx
@@ -1,7 +1,14 @@
 export default function Navbar(): JSX.Element {
     return(
         <nav>
-            
+            <div className="logo">Logo</div>
+            <div className="sections">
+                
+            </div>
+            <div className="buttons">
+                <button> SignUp</button>
+                <button>Login</button>
+            </div>
         </nav>
     );
 }
