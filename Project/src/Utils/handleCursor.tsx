162508e85d8c321a91f941113076d4fab5dbbd95
MOHIT DAVAR <mohitdavar2004@gmail.com>
2024-08-23 22:18:16 +0530

    Utils

---
+++ Project/src/Utils/handleCursor.tsx
@@ -0,0 +1,70 @@
+import React, { useLayoutEffect} from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const handleCursor = (
+    e: React.MouseEvent<HTMLDivElement>,
+    cursor: React.RefObject<HTMLDivElement>,
+    blur: React.RefObject<HTMLDivElement>
+) => {
+    const hoveredElement = e.target as HTMLElement;
+    if (hoveredElement.tagName.toLowerCase() === "a") {
+        if (cursor.current) {
+            cursor.current.classList.replace("size-3", "size-20");
+            gsap.to(cursor.current, {
+                backgroundColor: "transparent",
+            });
+        }
+        blur.current?.classList.add("hidden");
+    } else {
+        if (cursor.current) {
+            cursor.current.classList.replace("size-20", "size-3");
+            gsap.to(cursor.current, {
+                background: "#21d4fd",
+            });
+        }
+        blur.current?.classList.remove("hidden");
+    }
+    let cursorRect = cursor.current?.getBoundingClientRect() as DOMRect;
+    let blurRect = blur.current?.getBoundingClientRect() as DOMRect;
+    gsap.to(cursor.current, {
+        x: e.pageX - cursorRect.width / 2,
+        y: e.pageY - cursorRect.height / 2,
+        duration: 0.2,
+        ease: "power1.out",
+    });
+    gsap.to(blur.current, {
+        x: e.pageX - blurRect.width / 2,
+        y: e.pageY - blurRect.height / 2,
+        duration: 1,
+        ease: "sine.out",
+    });
+};
+useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const mainElement = document.querySelector("main");
+
+    if (mainElement) {
+        gsap.to(mainElement, {
+            background: "black",
+            scrollTrigger: {
+                trigger: mainElement,
+                scroller: "body",
+                scrub: true,
+                start: "top -25%",
+                end: "top -110%",
+                invalidateOnRefresh: true,
+                refreshPriority: -1,
+            },
+        });
+    }
+
+    ScrollTrigger.refresh();
+
+    return () => {
+        ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+}, []);
+
+export default handleCursor;
--- Project/src/components/Home/Home.tsx
+++ Project/src/components/Home/Home.tsx
@@ -1,80 +1,20 @@
-import { useLayoutEffect, useRef } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useRef } from "react";
 import Navbar from "../Includes/Navbar";
 import HeroSection from "./HeroSection/HeroSection";
 import MarqueeText from "./Marquee/MarqueeText";
 import TiltCards from "./TiltCards/TiltCards";
+import handleCursor from "../../Utils/handleCursor";
 
 function Home() {
     const cursor = useRef<HTMLDivElement>(null);
     const blur = useRef<HTMLDivElement>(null);
 
-    const handleCursor = (e: React.MouseEvent<HTMLDivElement>) => {
-        const hoveredElement = e.target as HTMLElement;
-        if (hoveredElement.tagName.toLowerCase() === "a") {
-            if (cursor.current) {
-                cursor.current.classList.replace("size-3", "size-20");
-                gsap.to(cursor.current, {
-                    backgroundColor: "transparent",
-                });
-            }
-            blur.current?.classList.add("hidden");
-        } else {
-            if (cursor.current) {
-                cursor.current.classList.replace("size-20", "size-3");
-                gsap.to(cursor.current, {
-                    background: "#21d4fd",
-                });
-            }
-            blur.current?.classList.remove("hidden");
-        }
-        let cursorRect = cursor.current?.getBoundingClientRect() as DOMRect;
-        let blurRect = blur.current?.getBoundingClientRect() as DOMRect;
-        gsap.to(cursor.current, {
-            x: e.pageX - cursorRect.width / 2,
-            y: e.pageY - cursorRect.height / 2,
-            duration: 0.2,
-            ease: "power1.out",
-        });
-        gsap.to(blur.current, {
-            x: e.pageX - blurRect.width / 2,
-            y: e.pageY - blurRect.height / 2,
-            duration: 1,
-            ease: "sine.out",
-        });
-    };
-    useLayoutEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
-
-        const mainElement = document.querySelector("main");
-
-        if (mainElement) {
-            gsap.to(mainElement, {
-                background: "black",
-                scrollTrigger: {
-                    trigger: mainElement,
-                    scroller: "body",
-                    scrub: true,
-                    start: "top -25%",
-                    end: "top -110%",
-                    invalidateOnRefresh: true,
-                    refreshPriority: -1,
-                },
-            });
-        }
-
-        ScrollTrigger.refresh();
-
-        return () => {
-            ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
-        };
-    }, []);
-
     return (
         <div
             className="mainContainer relative overflow-hidden selection:bg-white selection:text-themeTwo"
-            onMouseMove={handleCursor}
+            onMouseMove={(e: React.MouseEvent<HTMLDivElement>) => {
+                handleCursor(e, cursor, blur);
+            }}
         >
             <aside className="cursor-backgrounds">
                 <div
