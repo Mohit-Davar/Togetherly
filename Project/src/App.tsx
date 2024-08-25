32daa66934df845b50f9fe7aacaa6e3e1c9e32b5
MOHIT DAVAR <mohitdavar2004@gmail.com>
2024-08-25 23:54:32 +0530

    Duh

--- Project/src/App.tsx
+++ Project/src/App.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Includes/Navbar";
 function App() {
     return (
         <>
+            <Navbar/>
             <Outlet />
         </>
     );
